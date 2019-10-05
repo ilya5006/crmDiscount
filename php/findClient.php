a94c0291dbff5fd36d6cdcbad514072a241fdd8b
ilya5006 <samarcev.ilya@mail.ru>
2019-10-05 16:07:19 +0300

    FindClient.php

---
+++ php/findClient.php
@@ -0,0 +1,37 @@
+<?php
+    session_start();
+
+    if (!$_SESSION['isAuth'])
+    {
+        header("Location: ../auth.html");
+    }
+
+    require_once "./db.php";
+
+    function isUserFound($clientInfo)
+    {
+        if (isset($clientInfo['id_client']))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    $idClient = $_GET['id_client'];
+
+    $clientInfo = Database::query("SELECT * FROM clients WHERE id_client = '$idClient'");
+
+    if (isUserFound($clientInfo)) 
+    {
+        $_SESSION['clientInfo'] = $clientInfo;
+        header("Location: ../oneClient.php");
+    }
+    else
+    {
+        echo "Вы ввели неправильный ID";
+        unset($_SESSION['clientInfo']); // unset для того, чтобы очистить предыдущий результат поиска
+    }
+?>
