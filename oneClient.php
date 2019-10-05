9feb7b3f0c3c8d1cb436f2b57af87aa5201074fe
Alexander <mrreads@yandex.com>
2019-10-05 20:52:19 +0300

    design OneClient.php add

--- oneClient.php
+++ oneClient.php
@@ -13,8 +13,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./css/oneClient.css">
     <title>Информация о клиенте</title>
+    <link rel="stylesheet" href="./css/index.css">
 </head>
 <body>
     <header>
@@ -22,17 +22,17 @@
     </header>
 
     <div id="info">
-        <p id="fio"> ФИО: <?php echo $_SESSION['clientInfo']['fio']; ?> </p>
-        <p id="bonuses"> Количество бонусов: <?php echo $_SESSION['clientInfo']['bonuses']; ?> </p>
-        <p id="next_writeoff_date"> Следующая дата списания бонусов: <?php echo $_SESSION['clientInfo']['next_writeoff_date']; ?> </p>
+        <p id="fio"> <strong>ФИО:</strong> <br> <?php echo $_SESSION['clientInfo']['fio']; ?> </p>
+        <hr>
+        <p id="bonuses"> <strong>Количество бонусов:</strong> <br> <?php echo $_SESSION['clientInfo']['bonuses']; ?> </p>
+        <hr>
+        <p id="next_writeoff_date"> <strong>Следующая дата списания бонусов:</strong> <br> <?php echo $_SESSION['clientInfo']['next_writeoff_date']; ?> </p>
     </div>
 
     <form id="bonus_payment" action="./bonusPayment.php" method="GET">
         <input type="text" placeholder="Сумма заказа в рублях" name="sum">
         <input type="submit" value="Оплатить часть суммы бонусами">
     </form>
-
-    <hr>
 
     <form id="accrual_of_bonuses" action="./php/accrualOfBonuses.php" method="GET">
         <input type="text" placeholder="Сумма заказа в рублях" name="sum">
