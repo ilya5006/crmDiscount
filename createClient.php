3ab652bc066a560f713591570b9c7fcc9c6a5078
Alexander <mrreads@yandex.com>
2019-10-05 18:33:53 +0300

    design for index page

--- createClient.php
+++ createClient.php
@@ -14,10 +14,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Создать клиента</title>
+    <link rel="stylesheet" href="./css/index.css">
 </head>
 <body>
     <h1>Бонусы начисляются при минимальной сумме заказа 500 рублей</h1>
-
     <form action="./php/createClient.php" method="GET">
         <input type="text" name="fio" placeholder="Введите ФИО">
         <input type="text" name="amount_entered" placeholder="Введите сумму заказа (в рублях, только число)">
