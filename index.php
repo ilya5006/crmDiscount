e8826739043a107cd50d0a4b668090004280ab42
Alexander <mrreads@yandex.com>
2019-10-06 15:24:27 +0300

    search duplicate id

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
         <h1>Панель управления</h1>
     </header>
     <div id="main">
-        <form id="search" action="./php/findClient.php" method="GET">
+        <form id="search-form" action="./php/findClient.php" method="GET">
             <input type="text" placeholder="Введите id клиента" name="id_client" id="id_client">
             <input type="submit" value="Поиск" id="search">       <!-- Я не знаю, почему так, но если прописать ширину в .css-файле, то оно не работает... -->
         </form>
